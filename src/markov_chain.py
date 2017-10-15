7bcd5677e928eee284b79bc1349357d36565db9c
Sebastien (Work) <sebastien@cytora.com>
2017-10-15 23:31:19 +0100

    Simplify markov chain usage

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -5,8 +5,5 @@ with open('corpus_data.txt', encoding='utf-8') as corpus:
 
 text_model = markovify.Text(text)
 
-# for i in range(5):
-   # print(text_model.make_sentence())
-
 for i in range(10):
-    print(text_model.make_short_sentence(100))
+    print(text_model.make_short_sentence(120, tries=25))
