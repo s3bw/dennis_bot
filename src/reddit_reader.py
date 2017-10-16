956e39840471f988272e09eca63156ce30124e38
Sebastien (Work) <sebastien@cytora.com>
2017-10-16 18:29:30 +0100

    Created try and except for env vars locally and on heroku
    Similar to last commit
        this enables local testing and envi vars on heroku

--- src/reddit_reader.py
+++ src/reddit_reader.py
@@ -1,9 +1,10 @@
+import os
 import configparser
 
 import praw
 
 
-def auth_reddit():
+def auth_reddit_locally():
     config = configparser.ConfigParser()
     config.read('config.cfg')
 
@@ -21,13 +22,36 @@ def auth_reddit():
     )
     return reddit, config
 
+
+def auth_reddit_remotely():
+    CLIENT_ID = os.environ.get('REDDIT_ID')
+    CLIENT_SECRET = os.environ.get('REDDIT_SECRET')
+    PASSWORD = os.environ.get('REDDIT_PASSWORD')
+    USERNAME = os.environ.get('REDDIT_USER_NAME')
+
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        user_agent='make database with comments.',
+        password=PASSWORD,
+        username=USERNAME,
+    )
+    return reddit
+
+
 def research_corpus():
-    reddit, config = auth_reddit()
+    try:
+        reddit, config = auth_reddit_locally()
+    except:
+        reddit = auth_reddit_remotely()
 
     print('Reading Reddit:')
 
     corpus = []
-    subreddits = config.get('REDDIT', 'SUBREDDITS').split(',')
+    try:
+        subreddits = config.get('REDDIT', 'SUBREDDITS').split(',')
+    except:
+        subreddits = os.environ.get('REDDIT_SUBREDDITS').split(',')
 
     for subreddit in subreddits:
         for index, comment in enumerate(reddit.subreddit(subreddit).stream.comments()):
